5ab0d48543016dc9469cc4f7c6c492b8a813af87
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-12-27 23:55:20 +0900

    Add third solution

--- numpy_exercises/dshc.py
+++ numpy_exercises/dshc.py
@@ -4,6 +4,7 @@ import re
 
 def solution_one(A):
     # Must be less than 45 bytes
+    # OK: 41 bytes
     return np.stack(A[i : i + 7] for i in range(10))
 
 
@@ -15,5 +16,20 @@ def solution_two(A):
     # return re.sub('_', '', re.sub(r'o(?!_)', 't', re.sub('t', 'o_', A)))
     # NG: 48 bytes
     # return A.translate(str.maketrans({'t': 'o', 'o': 't'}))
+    # OK: 38 bytes
     # Ref: https://qiita.com/tag1216/items/df6c93bdb823dd48af6c
     return A.translate(str.maketrans('to', 'ot'))
+
+
+def solution_three(A):
+    # Must be less than 32 bytes
+    # NG: 51 bytes
+    # return ", ".join([s for s in A if re.match(r"[A-Z]+", s)])
+    # NG: 43 bytes
+    # return ", ".join([s for s in A if s[0].isupper()])
+    # NG: 40 bytes
+    # return ", ".join([s for s in A if s.istitle()])
+    # NG: 33 bytes
+    # return ", ".join(filter(str.istitle, A))
+    # OK? 32 bytes
+    return ", ".join(filter(str.istitle,A))
--- tests/test_numpy_exercises.py
+++ tests/test_numpy_exercises.py
@@ -15,3 +15,8 @@ def test_dshc_two():
         dshc.solution_two(A)
         == "ptsioitn ctnortl hand research soaff markeo cturo cturse evidence gtvernmeno view ptino oime man minisoer acoitn soaoe effeco mtoher ctunory"
     )
+
+
+def test_dshc_three():
+    A = ["country", "form", "Name", "order", "god", "information", "child", "Night", "policy", "development", "question", "end", "Work", "mother", "Problem", "Age", "Road", "Company", "Head", "home"]
+    assert (dshc.solution_three(A) == "Name, Night, Work, Problem, Age, Road, Company, Head")
